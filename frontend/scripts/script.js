c7ba239da9b2130a1c4830a02801be173a12aa44
Felipe dos Anjos <dosanjosfelipe06@gmail.com>
2025-03-13 16:15:53 -0300

    adding new features in header

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,3 +1,8 @@
+var light_logo = "../assets/header/umbrella-logo-light-theme.png"
+var dark_logo = "../assets/header/umbrella-logo-dark-theme.png"
+var light_theme_icon = "../assets/header/sun-icon.png"
+var dark_theme_icon = "../assets/header/moon-icon.png"
+
 function switchTheme() {
     // Pega o elemento body e header
     const body = document.body;
@@ -5,7 +10,8 @@ function switchTheme() {
     
     // Pega o elemento de imagem
     const themeIcon = document.getElementById('theme-icon');
-    
+    const logo = document.getElementById('logo-img')
+
     // Verifica qual tema está atualmente aplicado
     if (body.classList.contains('light-theme') && header.classList.contains('light-theme')) {
         // Muda para o tema escuro
@@ -13,8 +19,9 @@ function switchTheme() {
         body.classList.add('dark-theme');
         header.classList.remove('light-theme');
         header.classList.add('dark-theme');
-        themeIcon.src = '/frontend/assets/header/moon-icon.png';
+        themeIcon.src = dark_theme_icon;
         themeIcon.alt = 'Tema Escuro';
+        logo.src = dark_logo
         
         localStorage.setItem('theme', 'dark');
     } else {
@@ -23,29 +30,47 @@ function switchTheme() {
         body.classList.add('light-theme');
         header.classList.remove('dark-theme');
         header.classList.add('light-theme');
-        themeIcon.src = '/frontend/assets/header/sun-icon.png';
+        themeIcon.src = light_theme_icon;
         themeIcon.alt = 'Tema Claro';
-        
+        logo.src = light_logo
+
         localStorage.setItem('theme', 'light');
+        
     }
 }
 
-// Verificar se o usuário já tem uma preferência de tema
 window.onload = function() {
-    const savedTheme = localStorage.getItem('theme');
+    const currentPage = window.location.pathname;
+    const links = document.querySelectorAll('.navegation a');
     const body = document.body;
+    const savedTheme = localStorage.getItem('theme');
     const header = document.querySelector('header');
     const themeIcon = document.getElementById('theme-icon');
+    const logo = document.getElementById('logo-img');
+
+    //continuar a linha em baixo da pagina que esta
+    links.forEach(link => {
+        const linkHref = link.getAttribute('href');
+        if (currentPage.includes(linkHref)) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
     
-    if (savedTheme === 'dark') {
+    if (savedTheme === "dark"){
         body.classList.add('dark-theme');
         header.classList.add('dark-theme');
-        themeIcon.src = '/frontend/assets/header/moon-icon.png';
+        themeIcon.src = dark_theme_icon;
         themeIcon.alt = 'Tema Escuro';
+        logo.src = dark_logo
+
     } else {
         body.classList.add('light-theme');
         header.classList.add('light-theme');
-        themeIcon.src = '/frontend/assets/header/sun-icon.png';
+        themeIcon.src = light_theme_icon;
         themeIcon.alt = 'Tema Claro';
+        logo.src = light_logo
+
     }
 };
