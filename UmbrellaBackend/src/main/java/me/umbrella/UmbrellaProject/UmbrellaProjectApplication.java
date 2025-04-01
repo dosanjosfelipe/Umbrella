91d6070deb353fb74e4176d8f886a73281ba4510
Felipe dos Anjos <dosanjosfelipe06@gmail.com>
2025-04-01 13:24:32 -0300

    Adding SpringBoot

---
+++ UmbrellaBackend/src/main/java/me/umbrella/UmbrellaProject/UmbrellaProjectApplication.java
@@ -0,0 +1,13 @@
+package me.umbrella.UmbrellaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellaProjectApplication.class, args);
+	}
+
+}
---
+++ UmbrellaBackend/src/test/java/me/umbrella/UmbrellaProject/UmbrellaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.umbrella.UmbrellaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
