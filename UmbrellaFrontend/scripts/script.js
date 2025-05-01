0788b41c71c1ad882233906c03428d17745f45b7
Felipe dos Anjos <dosanjosfelipe06@gmail.com>
2025-05-01 18:20:55 -0300

    the final frontend version

--- UmbrellaFrontend/scripts/script.js
+++ UmbrellaFrontend/scripts/script.js
@@ -6,70 +6,70 @@ var dark_theme_icon = "../assets/header/moon-icon.png"
 function switchTheme() {
     // Pega o elemento body e header
     const body = document.body;
-    const header = document.querySelector('header');
+    const header = document.querySelector("header");
     
     // Pega o elemento de imagem
-    const themeIcon = document.getElementById('theme-icon');
-    const logo = document.getElementById('logo-img')
+    const themeIcon = document.getElementById("theme-icon");
+    const logo = document.getElementById("logo-img")
 
     // Verifica qual tema está atualmente aplicado
-    if (body.classList.contains('light-theme') && header.classList.contains('light-theme')) {
+    if (body.classList.contains("light-theme") && header.classList.contains("light-theme")) {
         // Muda para o tema escuro
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-        header.classList.remove('light-theme');
-        header.classList.add('dark-theme');
+        body.classList.remove("light-theme");
+        body.classList.add("dark-theme");
+        header.classList.remove("light-theme");
+        header.classList.add("dark-theme");
         themeIcon.src = dark_theme_icon;
-        themeIcon.alt = 'Tema Escuro';
+        themeIcon.alt = "Tema Escuro";
         logo.src = dark_logo
         
-        localStorage.setItem('theme', 'dark');
+        localStorage.setItem("theme", "dark");
     } else {
         // Muda para o tema claro
-        body.classList.remove('dark-theme');
-        body.classList.add('light-theme');
-        header.classList.remove('dark-theme');
-        header.classList.add('light-theme');
+        body.classList.remove("dark-theme");
+        body.classList.add("light-theme");
+        header.classList.remove("dark-theme");
+        header.classList.add("light-theme");
         themeIcon.src = light_theme_icon;
-        themeIcon.alt = 'Tema Claro';
+        themeIcon.alt = "Tema Claro";
         logo.src = light_logo
 
-        localStorage.setItem('theme', 'light');
+        localStorage.setItem("theme", "light");
         
     }
 }
 
 window.onload = function() {
     const currentPage = window.location.pathname;
-    const links = document.querySelectorAll('.navegation a');
+    const links = document.querySelectorAll(".navegation a");
     const body = document.body;
-    const savedTheme = localStorage.getItem('theme');
-    const header = document.querySelector('header');
-    const themeIcon = document.getElementById('theme-icon');
-    const logo = document.getElementById('logo-img');
+    const savedTheme = localStorage.getItem("theme");
+    const header = document.querySelector("header");
+    const themeIcon = document.getElementById("theme-icon");
+    const logo = document.getElementById("logo-img");
 
     //continuar a linha em baixo da pagina que esta
     links.forEach(link => {
-        const linkHref = link.getAttribute('href');
+        const linkHref = link.getAttribute("href");
         if (currentPage.includes(linkHref)) {
-            link.classList.add('active');
+            link.classList.add("active");
         } else {
-            link.classList.remove('active');
+            link.classList.remove("active");
         }
     });
     
     if (savedTheme === "dark"){
-        body.classList.add('dark-theme');
-        header.classList.add('dark-theme');
+        body.classList.add("dark-theme");
+        header.classList.add("dark-theme");
         themeIcon.src = dark_theme_icon;
-        themeIcon.alt = 'Tema Escuro';
+        themeIcon.alt = "Tema Escuro";
         logo.src = dark_logo
 
     } else {
-        body.classList.add('light-theme');
-        header.classList.add('light-theme');
+        body.classList.add("light-theme");
+        header.classList.add("light-theme");
         themeIcon.src = light_theme_icon;
-        themeIcon.alt = 'Tema Claro';
+        themeIcon.alt = "Tema Claro";
         logo.src = light_logo
 
     }
@@ -119,15 +119,15 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function updatePlaceholder() {
-    const input = document.getElementById('search_city');
+    const input = document.getElementById("search_city");
     if (window.innerWidth > 1000 && window.innerWidth < 1300) {
-      input.placeholder = 'Pesquisar';
+      input.placeholder = "Pesquisar";
     } else if (window.innerWidth < 1000) {
-      input.placeholder = '';
+      input.placeholder = "";
     } else {
-        input.placeholder = 'Pesquisar Local';
+        input.placeholder = "Pesquisar local";
     }
   }
   
-  window.addEventListener('resize', updatePlaceholder);
-  window.addEventListener('DOMContentLoaded', updatePlaceholder);
+  window.addEventListener("resize", updatePlaceholder);
+  window.addEventListener("DOMContentLoaded", updatePlaceholder);
